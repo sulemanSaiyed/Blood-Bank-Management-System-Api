265cfe39d08dec907f25cb38b7ba1178da23547f
pandu-12-2000 <crackivar@gmail.com>
2025-02-04 17:40:37 +0530

    intial commit

---
+++ src/main/java/Blood_Bank_Management_Api/BBM/BloodBankManagementApplication.java
@@ -0,0 +1,13 @@
+package Blood_Bank_Management_Api.BBM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodBankManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodBankManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Blood_Bank_Management_Api/BBM/BloodBankManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package Blood_Bank_Management_Api.BBM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodBankManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
