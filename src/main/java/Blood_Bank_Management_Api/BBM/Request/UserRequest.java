36dd847b52c8c287e217c18eee9b155cfab5b8a6
pandu-12-2000 <crackivar@gmail.com>
2025-03-07 22:27:09 +0530

    Create UserRequest class to achieve Data Transfer Object for user registration

---
+++ src/main/java/Blood_Bank_Management_Api/BBM/Request/UserRequest.java
@@ -0,0 +1,23 @@
+package Blood_Bank_Management_Api.BBM.Request;
+
+import Blood_Bank_Management_Api.BBM.enums.BloodGroup;
+import Blood_Bank_Management_Api.BBM.enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRequest {
+    private String userName;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private BloodGroup bloodGroup;
+    private int age;
+    private Gender gender;
+    private String availableCity;
+}
