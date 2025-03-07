6ad34ce5052a42107b58690cf67386ae6ac9aafe
pandu-12-2000 <crackivar@gmail.com>
2025-03-07 23:44:23 +0530

    updating srvice layer

--- src/main/java/Blood_Bank_Management_Api/BBM/Service/UserService.java
+++ src/main/java/Blood_Bank_Management_Api/BBM/Service/UserService.java
@@ -1,13 +1,15 @@
 package Blood_Bank_Management_Api.BBM.Service;
 
+import Blood_Bank_Management_Api.BBM.Request.UserRequest;
+import Blood_Bank_Management_Api.BBM.Response.UserResponse;
 import Blood_Bank_Management_Api.BBM.entity.User;
 
 public interface UserService {
 
-    User addUser(User user);
-    public User findByUserId(int userId);
+    UserResponse addUser(UserRequest userRequest);
+    public UserResponse findByUserId(int userId);
 
-    public User updateUserById(int userId, User user);
+    public User updateUserById(int userId, UserRequest userRequest);
 
 
 }
