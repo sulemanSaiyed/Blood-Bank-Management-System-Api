771edbe536471030f53fca3f1a7d37af155373f0
pandu-12-2000 <crackivar@gmail.com>
2025-03-07 22:16:28 +0530

    updaing controller class for more efficent response to the user by using response structure and builder method

--- src/main/java/Blood_Bank_Management_Api/BBM/Controller/UserController.java
+++ src/main/java/Blood_Bank_Management_Api/BBM/Controller/UserController.java
@@ -3,19 +3,40 @@ package Blood_Bank_Management_Api.BBM.Controller;
 import Blood_Bank_Management_Api.BBM.Service.UserService;
 import Blood_Bank_Management_Api.BBM.entity.User;
 import Blood_Bank_Management_Api.BBM.utility.ResponseStructure;
-import org.springframework.beans.factory.annotation.Autowired;
+import Blood_Bank_Management_Api.BBM.utility.RestResponseBuilder;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
 
 public class UserController {
-    @Autowired
-    UserService userService;
 
+   private final UserService userService;
+   private final RestResponseBuilder restResponseBuilder;
+
+@PostMapping("/register")
     public ResponseEntity<ResponseStructure<User>> addUser(@RequestBody User user){
 user=userService.addUser(user);
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(ResponseStructure.create(HttpStatus.CREATED.value(),"User created", user) );
+        return restResponseBuilder
+                .success(HttpStatus.CREATED,"user created", user) ;
     }
+
+    @GetMapping("/users/{userid}")
+    public ResponseEntity<ResponseStructure<User>>findByUserId(@PathVariable  ("userid") int userId) {
+User user= userService.findByUserId(userId);
+return restResponseBuilder.
+        success(HttpStatus.FOUND, "User Found", user);
+    }
+@PutMapping("/users/{userid}")
+    public ResponseEntity<ResponseStructure<User>>updateUser(@PathVariable ("userid") int userId, @RequestBody User user1) {
+    user1=userService.updateUserById(userId,user1);
+    return restResponseBuilder.success(HttpStatus.OK,"user updated", user1 );
+
 }
+}
+
+
