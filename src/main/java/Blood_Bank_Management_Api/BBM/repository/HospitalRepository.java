2c7862c6f0fd87d1710ce084bcbc6bc8e323c5a3
pandu-12-2000 <crackivar@gmail.com>
2025-03-24 20:36:45 +0530

    creating hospital repositry

---
+++ src/main/java/Blood_Bank_Management_Api/BBM/repository/HospitalRepository.java
@@ -0,0 +1,9 @@
+package Blood_Bank_Management_Api.BBM.repository;
+
+import Blood_Bank_Management_Api.BBM.entity.Hospital;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HospitalRepository extends JpaRepository<Hospital, Integer > {
+}
