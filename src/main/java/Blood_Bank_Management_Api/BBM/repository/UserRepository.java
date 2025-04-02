43adaf45829f403e107a15164f876602fc13c76e
pandu-12-2000 <crackivar@gmail.com>
2025-04-02 15:32:57 +0530

    he repository layer is like the part of your application responsible for directly talking to the database. The method  findbyEmal is added there because it’s all about retrieving data from the database, which is the repository’s job

--- src/main/java/Blood_Bank_Management_Api/BBM/repository/UserRepository.java
+++ src/main/java/Blood_Bank_Management_Api/BBM/repository/UserRepository.java
@@ -2,5 +2,11 @@ package Blood_Bank_Management_Api.BBM.repository;
 
 import Blood_Bank_Management_Api.BBM.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
+    Optional<User>findByEmail(String userName);
 }
