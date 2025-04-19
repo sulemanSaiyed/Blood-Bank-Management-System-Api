ac6ddca58e7b1437cd909aeec3eb36c5be314218
pandu-12-2000 <crackivar@gmail.com>
2025-04-19 18:31:22 +0530

    creating admin entityy

---
+++ src/main/java/Blood_Bank_Management_Api/BBM/entity/Admin.java
@@ -0,0 +1,29 @@
+package Blood_Bank_Management_Api.BBM.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int adminId;
+
+
+    @OneToOne
+    private User user;
+
+    @ManyToOne
+    private BloodBank bloodBank;
+
+    @ManyToOne
+    private Hospital hospital;
+}
