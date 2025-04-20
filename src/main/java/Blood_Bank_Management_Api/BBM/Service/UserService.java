df5d1fe96c8827e418c6e37068c60413597d619c
pandu-12-2000 <crackivar@gmail.com>
2025-04-21 01:09:19 +0530

    upadting userservice for  add user as admind

--- src/main/java/Blood_Bank_Management_Api/BBM/Service/UserService.java
+++ src/main/java/Blood_Bank_Management_Api/BBM/Service/UserService.java
@@ -3,6 +3,7 @@ package Blood_Bank_Management_Api.BBM.Service;
 import Blood_Bank_Management_Api.BBM.Request.UserRequest;
 import Blood_Bank_Management_Api.BBM.Response.UserResponse;
 import Blood_Bank_Management_Api.BBM.entity.User;
+import jakarta.validation.Valid;
 
 public interface UserService {
 
@@ -10,7 +11,7 @@ public interface UserService {
     public UserResponse findByUserId(int userId);
 
     public UserResponse updateUserById(int userId, UserRequest userRequest);
-
+    UserResponse addUserAsAdmin(@Valid UserRequest userRequest);
 
 }
   
