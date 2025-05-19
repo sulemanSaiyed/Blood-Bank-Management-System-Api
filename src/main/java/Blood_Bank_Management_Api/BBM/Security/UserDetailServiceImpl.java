a624534f31e0374c4123b188287c23d5466a6761
pandu-12-2000 <crackivar@gmail.com>
2025-05-19 22:51:41 +0530

    - Created UserDetailServiceImpl implementing UserDetailsService. - Loads user from database using UserRepository by email. - Returns Spring Security UserDetails object with username and password. - Throws custom BloodBankNotFoundByIDException if user not found.

---
+++ src/main/java/Blood_Bank_Management_Api/BBM/Security/UserDetailServiceImpl.java
@@ -0,0 +1,30 @@
+package Blood_Bank_Management_Api.BBM.Security;
+
+import Blood_Bank_Management_Api.BBM.Exception.BloodBankNotFoundByIDException;
+import Blood_Bank_Management_Api.BBM.entity.User;
+import Blood_Bank_Management_Api.BBM.repository.UserRepository;
+import lombok.AllArgsConstructor;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserDetailServiceImpl implements UserDetailsService{
+
+private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+       User user= userRepository.findByEmail(username).orElseThrow(()-> new BloodBankNotFoundByIDException("failed to find the user"));
+
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+
+                .build();
+    }
+}
