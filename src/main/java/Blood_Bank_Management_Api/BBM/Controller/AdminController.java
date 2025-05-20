2b915db09b5a6dd360d5902dd04e460dc25352fa
pandu-12-2000 <crackivar@gmail.com>
2025-05-20 07:37:15 +0530

    updating as much uopdated need to fix admincontroller

--- src/main/java/Blood_Bank_Management_Api/BBM/Controller/AdminController.java
+++ src/main/java/Blood_Bank_Management_Api/BBM/Controller/AdminController.java
@@ -1,6 +1,8 @@
 package Blood_Bank_Management_Api.BBM.Controller;
 
+import Blood_Bank_Management_Api.BBM.Request.UserRequest;
 import Blood_Bank_Management_Api.BBM.Response.AdminResponse;
+import Blood_Bank_Management_Api.BBM.Response.UserResponse;
 import Blood_Bank_Management_Api.BBM.Service.AdminService;
 import Blood_Bank_Management_Api.BBM.utility.ResponseStructure;
 import Blood_Bank_Management_Api.BBM.utility.RestResponseBuilder;
@@ -9,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,9 +22,9 @@ public class  AdminController {
     private final RestResponseBuilder responseBuilder;
 
     @PostMapping("/admins/{userId}")
-    public ResponseEntity<ResponseStructure<AdminResponse>> promoteUserToAdmin(@PathVariable int userId) {
-        AdminResponse adminResponse = adminService.promoteUserToAdmin(userId);
-        return responseBuilder.success(HttpStatus.CREATED, "Admin Created", adminResponse);
+    public ResponseEntity<ResponseStructure<AdminResponse>> promoteUserToAdmin(@RequestBody UserRequest userRequest, @PathVariable int userId) {
+        UserResponse userResponse = adminService.promoteUserToAdmin(userRequest,userId);
+        return responseBuilder.success(HttpStatus.CREATED, "Admin Created", userResponse);
 
     }
 
