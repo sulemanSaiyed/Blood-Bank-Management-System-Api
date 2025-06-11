33f1ad7725ccb66cc79aaad9aaf95a0844c01703
pandu-12-2000 <crackivar@gmail.com>
2025-06-12 00:14:28 +0530

    updating blood bank repository to creat custom method to find blood bank by city

--- src/main/java/Blood_Bank_Management_Api/BBM/repository/BloodBankRepository.java
+++ src/main/java/Blood_Bank_Management_Api/BBM/repository/BloodBankRepository.java
@@ -4,5 +4,8 @@ import Blood_Bank_Management_Api.BBM.entity.BloodBank;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BloodBankRepository extends JpaRepository<BloodBank, Integer> {
+    public List<BloodBank> findByAddress_CityIn(List<String> cities);
 }
